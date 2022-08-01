dea9c2faecde20e58cccc1dc03f8d5d8d3b31089
Alexandr <shabalin@voltsbattery.com>
2022-08-01 23:03:52 +0300

    Third task

---
+++ Chapter_2/3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    int age = 24;
+    int age_days = age * 365;
+    printf("age = %u \n", age);
+    printf("days = %u \n", age_days);
+    return 0;
+}
