3b79a1c7f72e549b9faf3676daa927433f814bae
Alexandr <shabalin@voltsbattery.com>
2022-08-01 23:10:48 +0300

    Seventh task

---
+++ Chapter_4/7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <locale.h>
+#include <float.h>
+
+int main()
+{
+    double a = 1.0 / 3.0;
+    float b = 1.0 / 3.0;
+    printf("\n%.4f\t%.4f", a, b);
+    printf("\n%.12f\t%.12f", a, b);
+    printf("\n%.16f\t%.16f", a, b);
+    printf("\n%d\t%d", FLT_DIG, DBL_DIG);
+}
