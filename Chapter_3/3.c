f124f9adb2239cdde0615d36425f57888212e1ac
Alexandr <shabalin@voltsbattery.com>
2022-08-01 23:08:16 +0300

    Third task

---
+++ Chapter_3/3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <locale.h>
+#include <limits.h>
+#include <float.h>
+
+int main(void)
+{
+    printf ("\a Напуганная внезапным звуком, Вика вскрикнула: \n \"Во имя всех звезд, что это было!\""); // \a - предупредительный звук (стандарт ANSI C)
+}
