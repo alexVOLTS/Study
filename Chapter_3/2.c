3412a8b062333af6bc22814ea7794bbe86e8ec5f
Alexandr <shabalin@voltsbattery.com>
2022-08-01 23:08:10 +0300

    Second task

---
+++ Chapter_3/2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <locale.h>
+#include <limits.h>
+#include <float.h>
+
+int main(void)
+{
+    
+    char ascii;
+    printf("Enter ASCII code:\n");
+    scanf("%d", &ascii);
+
+    printf("ASCII code for %d is %c", ascii, ascii);
+    getchar();
+    getchar();
+
+    return 0;
+
+}
