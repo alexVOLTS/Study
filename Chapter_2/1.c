1416acf2ecb448713c5c7c7840f2d459f44a419e
Alexandr <shabalin@voltsbattery.com>
2022-08-01 23:03:30 +0300

    First task

---
+++ Chapter_2/1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    printf ("Шабалин Александр");
+    printf ("\nАлександр" "\nShabalin");
+    printf ("\nШабалин");
+    printf ("\tАлександр");
+    return 0;
+}
