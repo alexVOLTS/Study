d5d68059196dc169ddff4f3ec49063fc0834101a
Alexandr <shabalin@voltsbattery.com>
2022-08-01 23:04:00 +0300

    Fourth task

---
+++ Chapter_2/4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <locale.h>
+
+int jolly()
+{
+    printf ("\nОн веселый молодец!");
+}
+int deny ()
+{
+    printf ("\nНикто не может это отрицать!");
+}
+
+int main()
+{
+    jolly();
+    jolly();
+    jolly();
+    deny();
+}
