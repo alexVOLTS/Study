843a12d17a8f49327fb773e3e285ef823f42c2f5
Alexandr <shabalin@voltsbattery.com>
2022-08-02 21:03:40 +0300

    Fourth task

---
+++ Chapter_5/4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <locale.h>
+#include <float.h>
+
+#define foot 0.0328084;
+#define inch 0.393701;
+
+float cm;
+
+int input()
+{
+    printf("\nInput height in centimeters:\t");
+    scanf("%f", &cm); 
+    return cm;
+}
+
+int cycle()
+{
+    while (cm > 0)
+    {
+        float x = cm * foot;
+        float y = cm * inch;
+        printf("\n%.1f centimeters = %.0f foots, %.1f inches (<=0 to exit from the program)", cm, x, y);
+        input ();
+    }
+    printf ("Program ends now.");
+}
+
+int main(void)
+{
+    input();
+    cycle();
+    return 0;
+}
