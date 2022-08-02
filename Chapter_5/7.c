c56f0a405a4f325e578e95ae94f19fd2d703b8a4
Alexandr <shabalin@voltsbattery.com>
2022-08-02 21:04:10 +0300

    Seneth\ task

---
+++ Chapter_5/7.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <locale.h>
+#include <float.h>
+
+double x;
+
+int calculate ()
+{
+    printf("\nInput number with double type: ");
+    scanf("%lf", &x);
+    x = x*x*x;
+    return x;
+}
+
+int main (void)
+{
+    calculate();
+    printf("\nResult: %f", x);
+    return 0;
+}
