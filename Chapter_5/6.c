13d9b71b1a5521abc08aa875d1488a98063e1273
Alexandr <shabalin@voltsbattery.com>
2022-08-02 21:04:02 +0300

    Sixth task

---
+++ Chapter_5/6.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <locale.h>
+#include <float.h>
+
+
+int main (void)
+{
+    int count, sum, num;
+    count = 0; 
+    sum = 0; 
+    printf("\nInput number:");
+    scanf("%d", &num);
+    while (count++ < num)
+    {
+        sum = sum + count*count;
+        printf("sum = %i\n", sum);
+    }
+    return 0;
+}
