1105076ad1f502bf5cab44dc2e0e47bfdc27bb5e
Alexandr <shabalin@voltsbattery.com>
2022-08-09 20:18:17 +0300

    task 5.9 is done

--- Chapter_5/9.c
+++ Chapter_5/9.c
@@ -6,34 +6,32 @@ double x;
 double y;
 double temp;
 int scanfresult;
+const double kelvin = 273.16;
+const double f1 = 5.0 / 9.0;
+const double f2 = 32.0;
 
 
 int Temperatures()
 {
-    const double kelvin = 273.16;
-    const double f1 = 5.0 / 9.0;
-    const double f2 = 32.0;
     double x = f1 * (temp - f2);
     double y = x + kelvin;
-    printf ("Numbers %.2lf and %.2lf", x, y);
-    return x, y;
+    printf("\nTemperature Celsium = %.2lf", x);
+    printf("\nTemperature Kelvin = %.2lf", y);
 }
 
 int Input ()
 {
     printf("\nInput fahrenheit temperature: ");
     scanfresult = scanf("%lf", &temp);
-    return temp, scanfresult;
+    return scanfresult;
 }
 
-int main (void)
+int main ()
 {
     Input();
-    Temperatures();
     while (scanfresult == 1)
     {
-        printf("\nTemperature Celsium = %.2lf", x);
-        printf("\nTemperature Kelvin = %.2lf", y);
+        Temperatures();
         Input();
     }
     printf("\nEnd of the program");
