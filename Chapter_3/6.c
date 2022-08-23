c489e8dba7193f04912c4df699c7b80a071ced00
Alexandr <shabalin@voltsbattery.com>
2022-08-23 21:36:30 +0300

    Resolved magic numbers in Task 3.6

--- Chapter_3/6.c
+++ Chapter_3/6.c
@@ -3,14 +3,18 @@
 #include <limits.h>
 #include <float.h>
 
+#define WEIGHT     950.0f
+#define MOLECUL    3.0E-23f
+
 int main(void)
 {
     float quart;
+
     printf("Input water volume in quarts:\t");
     scanf("%f", &quart);
-    float weight = 950;
-    float molecul = 3.0E-23;
-    float number = (quart * weight) / molecul;
+
+    float number = (quart * WEIGHT) / MOLECUL;
+
     printf("\n Water molecules number: %f", number);
     return 0;
 }
