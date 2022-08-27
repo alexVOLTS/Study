81f89d63d5eb057c673c9956b21ffd954a661ba3
Alexandr Shabalin <shabalin@voltsbattery.com>
2022-08-27 20:33:19 +0300

    Added Task 6.15

---
+++ Chapter_6/6_15.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <locale.h>
+#include <float.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define SIZE    256u
+
+int main(void)
+{
+    char str[SIZE] = "";
+    
+    printf("\nInput string: ");
+    scanf("%255s", &str);
+    
+    printf("\nReversed string: ");
+    for (int i = 0; i <= SIZE; i++){
+        printf("%c", str[(SIZE - 1) - i]);
+    }
+    
+    return 0;
+}
