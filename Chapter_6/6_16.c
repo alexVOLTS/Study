aeab4980453530d8bc3faaf20ed1f98c55beb86c
Alexandr Shabalin <shabalin@voltsbattery.com>
2022-08-27 21:20:24 +0300

    Added counter of years in Task 6.16

--- Chapter_6/6_16.c
+++ Chapter_6/6_16.c
@@ -13,12 +13,15 @@ int main(void)
     float dafna = 0.0f;
     float deidra = 0.0f;
     int deidras_deposit = 100;
+    int counter = 0;
     
     do {
         dafna += DEPOSIT_FOR_DAFNA + (DEPOSIT_FOR_DAFNA * PERCENT_FOR_DAFNA);
         deidras_deposit += (deidras_deposit * PERCENT_FOR_DEIDRA);
         deidra += deidras_deposit;
+	    counter++;
     } while (deidra <= dafna);
     printf("\nDafna's deposit is %f", dafna);
     printf("\nDeidra's deposit is %f", deidra);
+    printf("\nYears to Deidra have more BUCKS than Dafna do: %d", counter);
 }
