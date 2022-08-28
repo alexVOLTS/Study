d73678392c136241c1965ac4a23448b7f4249460
Alexandr Shabalin <shabalin@voltsbattery.com>
2022-08-28 16:46:11 +0300

    Deleted locale.h in Task Lesson#1_4

--- Lesson#1/Tasks/Lesson#1_4.c
+++ Lesson#1/Tasks/Lesson#1_4.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
-#include <locale.h>
 #include <string.h>
-
-
 
 int main(void)
 {
     unsigned int number, revers = 0;
+
     printf("\nInput integer number: ");
     scanf("%d", &number);
+
     while (number != 0)
     {
         revers = revers * 10 + number % 10;
         number = number / 10;
     }
-    printf("\nReversed number is:%d", revers);
+    printf("\nReversed number is: %d", revers);
     return 0;
 }
