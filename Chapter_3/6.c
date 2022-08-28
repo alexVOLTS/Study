6c766c0cb1847f0ba92e45dcd436a22094578f25
Alexandr Shabalin <shabalin@voltsbattery.com>
2022-08-28 17:09:08 +0300

    Changed #define name and number from float to double in Task 3.6

--- Chapter_3/6.c
+++ Chapter_3/6.c
@@ -3,8 +3,8 @@
 #include <limits.h>
 #include <float.h>
 
-#define WEIGHT     950.0f
-#define MOLECUL    3.0E-23f
+#define WEIGHT               950.0f
+#define MOLECULS_IN_WATER    3.0E-23
 
 int main(void)
 {
@@ -13,7 +13,7 @@ int main(void)
     printf("Input water volume in quarts:\t");
     scanf("%f", &quart);
 
-    float number = (quart * WEIGHT) / MOLECUL;
+    float number = (quart * WEIGHT) / MOLECULS_IN_WATER;
 
     printf("\n Water molecules number: %f", number);
     return 0;
