400a5ef1132795aa8e04e0214da13b849c3bbba9
Alexandr Shabalin <shabalin@voltsbattery.com>
2022-09-03 21:45:05 +0300

    Added Task 7.2

---
+++ Chapter_7/7_2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <locale.h>
+#include <float.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define SIZE_OF_STRING    8u
+#define GRID_ASCII        35u
+
+int main(void)
+{
+    char symbol;
+    int new_string;
+    int symbols_counter = 0;
+    
+    while((symbol = getchar()) != GRID_ASCII){
+        
+        symbols_counter++;
+        new_string = (symbols_counter % SIZE_OF_STRING);
+        printf("%c ", symbol);
+        printf("%d ", symbol);
+          
+        if (new_string == 0){
+            printf("\n");
+        }
+    }
+
+    return 0;
+}
