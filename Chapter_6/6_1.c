fa21c3c0c22bc616fe813937b1ea0804ceda113b
Alexandr Shabalin <shabalin@voltsbattery.com>
2022-09-07 22:29:04 +0300

    Fixed spaces in Task 6.1

--- Chapter_6/6_1.c
+++ Chapter_6/6_1.c
@@ -8,7 +8,8 @@ int main(void)
     char abc[27] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', //
     'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', //
     'v', 'w', 'x', 'y', 'z'};
-    for (int i = 0; i < sizeof(abc); i++){
+	
+    for (int i = 0; i < sizeof(abc); i++) {
         printf("%c", abc[i]);
     }
     return 0;
