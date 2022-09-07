3f5cfd518eb51703e15337bfa02438625d3a5e4b
Alexandr Shabalin <shabalin@voltsbattery.com>
2022-09-07 22:28:29 +0300

    Fixed spaces and void as function input return type in Task 6.10

--- Chapter_6/6_10.c
+++ Chapter_6/6_10.c
@@ -7,10 +7,11 @@
 int a, b;
 int c = 0;
 
-int input()
+void input()
 {
     printf("\nInput low number: ");
     scanf("%d", &a);
+	
     printf("\nInput high number: ");
     scanf("%d", &b);
 }
@@ -20,15 +21,16 @@ int main(void)
     input();
     int d = a;
     
-    while (a < b){
+    while (a < b) {
         for (a; a <= b; a++){
             c = c + (a * a);
-            // a++;
         }
         printf("\nSum of squares of int numbers from %d to %d is %d ", d, b, c);
         c = 0;
         input();
     }
+	
     printf("\n\tEnd of the program...");
+	
     return 0;
 }
