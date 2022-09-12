2afd354b2bd8b72e2e34a64793de3f8a3d23e1eb
alexVOLTS <shabalin@voltsbattery.com>
2022-09-12 16:17:12 +0300

    Deleted extra tasks

--- Chapter_2/2.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <locale.h>
-
-int main(void)
-{
-    printf("Имя: Alexandr \t Адрес: Somewhere in the World 1664");
-    return 0;
-}
--- Chapter_3/3.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <locale.h>
-#include <limits.h>
-#include <float.h>
-
-int main(void)
-{
-    printf ("\a Scared by random sound Vika screamed: \n \"In the name of all the stars what it was!\""); // \a - sound (ANSI C Standard)
-}
